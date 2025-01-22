cabc99f8574cdbe5027c75364a2e02e38d569ec0
Anhgelus Morhtuuzh <anhgelus@anhgelus.world>
2025-01-22 11:48:05 +0100

    feat(lexer): simple

---
+++ lexer.go
@@ -0,0 +1,77 @@
+package main
+
+import (
+	"errors"
+	"slices"
+	"strconv"
+	"strings"
+)
+
+const (
+	literal   string = "literal"
+	number    string = "number"
+	seperator string = "seperator"
+	operator  string = "operator"
+)
+
+var (
+	InvalidSeparatorErr          = errors.New("invalid separator")
+	operators           []string = []string{"+", "-", "*", "/", "^", "%", "="}
+)
+
+type Lexer struct {
+	Type  string
+	Value string
+}
+
+func lex(content []string) ([]Lexer, error) {
+	var lexers []Lexer
+	for _, line := range content {
+		for _, w := range strings.Split(line, " ") {
+			word, err := lexWord(w)
+			if err != nil {
+				return nil, err
+			}
+			lexers = append(lexers, word...)
+		}
+	}
+	return lexers, nil
+}
+
+func lexWord(w string) ([]Lexer, error) {
+	if isDigit(w) {
+		return []Lexer{
+			{number, w},
+		}, nil
+	}
+	if strings.Contains(w, ",") {
+		if strings.Contains(w[:len(w)-1], ",") {
+			return nil, errors.Join(InvalidSeparatorErr, errors.New(w+" contains more than one comma"))
+		}
+		return []Lexer{
+			{literal, w[:len(w)-1]},
+			{seperator, ","},
+		}, nil
+	}
+	if isOperator(w) {
+		return []Lexer{
+			{operator, w},
+		}, nil
+	}
+	return []Lexer{
+		{literal, w},
+	}, nil
+}
+
+func isDigit(s string) bool {
+	_, err := strconv.ParseFloat(s, 64)
+	return err == nil
+}
+
+func isOperator(s string) bool {
+	return slices.Contains(operators, s)
+}
+
+func (l *Lexer) String() string {
+	return l.Type + "(" + l.Value + ")"
+}
--- main.go
+++ main.go
@@ -1,5 +1,31 @@
 package main
 
+import (
+	"flag"
+	"os"
+	"strings"
+)
+
+var (
+	input string
+)
+
+func init() {
+	flag.StringVar(&input, "i", "", "input file")
+}
+
 func main() {
-	print("Hello World")
+	flag.Parse()
+	b, err := os.ReadFile(input)
+	if err != nil {
+		panic(err)
+	}
+	content := strings.Split(string(b), "\n")
+	lexed, err := lex(content)
+	if err != nil {
+		panic(err)
+	}
+	for _, l := range lexed {
+		println(l.String())
+	}
 }

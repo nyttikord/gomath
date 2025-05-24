871a6067654723e3cf327cae93337e703d8ec249
Anhgelus Morhtuuzh <anhgelus@anhgelus.world>
2025-05-24 19:13:24 +0200

    test(lexer): simple unary

--- lexer_test.go
+++ lexer_test.go
@@ -71,6 +71,8 @@ func TestLexerSub(t *testing.T) {
 	fn := func(l []*lexer) {
 		if len(l) != 3 {
 			t.Errorf("lexer has wrong length, got %d, excepted %d", len(lexr), 3)
+			printLex(t, lexr)
+			return
 		}
 		first := l[0]
 		second := l[1]
@@ -117,6 +119,43 @@ func TestLexerSub(t *testing.T) {
 		printLex(t, lexr)
 		t.Fatal(err)
 	}
+}
+
+func TestLexerUnary(t *testing.T) {
+	lexr, err := lex("+1")
+	if err != nil {
+		t.Fatal(err)
+	}
+	fn := func(l []*lexer, op string) {
+		if len(l) != 2 {
+			t.Errorf("lexer has wrong length, got %d, excepted %d", len(lexr), 2)
+			printLex(t, lexr)
+			return
+		}
+		first := l[0]
+		second := l[1]
+		if first.Type != Operator {
+			t.Errorf("got type %s; want Operator", first.Type)
+		}
+		if first.Value != op {
+			t.Errorf("got %s; want %s", first.Value, op)
+		}
+		if second.Type != Number {
+			t.Errorf("got type %s; want Number", second.Type)
+		}
+		if second.Value != "1" {
+			t.Errorf("got %s; want 1", second.Value)
+		}
+		if t.Failed() {
+			printLex(t, lexr)
+		}
+	}
+	fn(lexr, "+")
+	lexr, err = lex("-1")
+	if err != nil {
+		t.Fatal(err)
+	}
+	fn(lexr, "-")
 }
 
 func printLex(t *testing.T, lexr []*lexer) {

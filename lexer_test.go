9df82088bf7bb541cee3ba03c3a43e7272127c6b
Anhgelus Morhtuuzh <anhgelus@anhgelus.world>
2025-05-24 19:05:37 +0200

    test(lexer): simple substraction

--- lexer_test.go
+++ lexer_test.go
@@ -5,7 +5,7 @@ import (
 	"testing"
 )
 
-func TestLexerAdd(t *testing.T) {
+func TestLexerSum(t *testing.T) {
 	lexr, err := lex("1+2")
 	if err != nil {
 		t.Fatal(err)
@@ -13,6 +13,8 @@ func TestLexerAdd(t *testing.T) {
 	fn := func(l []*lexer) {
 		if len(l) != 3 {
 			t.Errorf("lexer has wrong length, got %d, excepted %d", len(lexr), 3)
+			printLex(t, lexr)
+			return
 		}
 		first := l[0]
 		second := l[1]
@@ -35,6 +37,9 @@ func TestLexerAdd(t *testing.T) {
 		if third.Value != "2" {
 			t.Errorf("got %s; want 2", third.Value)
 		}
+		if t.Failed() {
+			printLex(t, lexr)
+		}
 	}
 	t.Log("Testing 1+2")
 	fn(lexr)
@@ -53,6 +58,71 @@ func TestLexerAdd(t *testing.T) {
 	t.Log("Testing error 1 +2")
 	lexr, err = lex("1 +2")
 	if !errors.Is(err, SameTypeFollowErr) {
+		printLex(t, lexr)
 		t.Fatal(err)
 	}
+}
+
+func TestLexerSub(t *testing.T) {
+	lexr, err := lex("1-2")
+	if err != nil {
+		t.Fatal(err)
+	}
+	fn := func(l []*lexer) {
+		if len(l) != 3 {
+			t.Errorf("lexer has wrong length, got %d, excepted %d", len(lexr), 3)
+		}
+		first := l[0]
+		second := l[1]
+		third := l[2]
+		if first.Type != Number {
+			t.Errorf("got type %s; want Number", first.Type)
+		}
+		if first.Value != "1" {
+			t.Errorf("got %s; want 1", first.Value)
+		}
+		if second.Type != Operator {
+			t.Errorf("got type %s; want Operator", second.Type)
+		}
+		if second.Value != "-" {
+			t.Errorf("got %s; want -", second.Value)
+		}
+		if third.Type != Number {
+			t.Errorf("got type %s; want Number", third.Type)
+		}
+		if third.Value != "2" {
+			t.Errorf("got %s; want 2", third.Value)
+		}
+		if t.Failed() {
+			printLex(t, lexr)
+		}
+	}
+	t.Log("Testing 1-2")
+	fn(lexr)
+	t.Log("Testing 1 - 2")
+	lexr, err = lex("1 - 2")
+	if err != nil {
+		t.Fatal(err)
+	}
+	fn(lexr)
+	t.Log("Testing 1- 2")
+	lexr, err = lex("1- 2")
+	if err != nil {
+		t.Fatal(err)
+	}
+	fn(lexr)
+	t.Log("Testing error 1 -2")
+	lexr, err = lex("1 -2")
+	if !errors.Is(err, SameTypeFollowErr) {
+		printLex(t, lexr)
+		t.Fatal(err)
+	}
+}
+
+func printLex(t *testing.T, lexr []*lexer) {
+	s := ""
+	for _, l := range lexr {
+		s += l.String() + " "
+	}
+	t.Log(s[:len(s)-1])
 }

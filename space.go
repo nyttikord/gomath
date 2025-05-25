397580f33333c0b1a2b6a3a5fe1f769e2b5cb2fc
Léo-21 <leo.kosman@proton.me>
2025-05-25 20:26:00 +0200

    feat(space): add real intervals, periodic intervals and union sets

--- space.go
+++ space.go
@@ -1,12 +1,7 @@
 package gomath
 
 import (
-	"errors"
 	"fmt"
-)
-
-var (
-	ErrUnknownSpace = errors.New("unknown space")
 )
 
 type Space interface {
@@ -15,59 +10,83 @@ type Space interface {
 }
 
 type RealSet struct{}
-
-type RationalSet struct{}
-
-type RelativeSet struct{}
-
-type NaturalSet struct{}
-
-func parseSpace(s string) (Space, error) {
-	switch s {
-	case "R":
-		return RealSet{}, nil
-	case "Q":
-		return RationalSet{}, nil
-	case "Z":
-		return RelativeSet{}, nil
-	case "N":
-		return NaturalSet{}, nil
-	}
-	return nil, errors.Join(ErrUnknownSpace, fmt.Errorf("unknown space %s", s))
+type RealInterval struct {
+	lowerBound *fraction
+	upperBound *fraction
+	customName string
+}
+type UnionSet struct {
+	sets       []Space
+	customName string
 }
 
-func (RealSet) Contains(f *fraction) bool {
+type PeriodicInterval struct {
+	interval   RealInterval
+	period     *fraction
+	customName string
+}
+
+func (RealSet) Contains(*fraction) bool {
 	return true
 }
-
 func (RealSet) String() string {
 	return "R"
 }
 
-func (RationalSet) Contains(f *fraction) bool {
+func (set RealInterval) Contains(f *fraction) bool {
+	return f.SmallerOrEqualThan(set.upperBound) && f.GreaterOrEqualThan(set.upperBound)
+}
+func (set RealInterval) String() string {
+	if set.customName != "" {
+		return set.customName
+	}
+	return fmt.Sprintf("[%s, %s]", set.lowerBound.String(), set.upperBound.String())
+}
+
+func (set *UnionSet) Contains(f *fraction) bool {
+	for _, space := range set.sets {
+		if !space.Contains(f) {
+			return false
+		}
+	}
 	return true
 }
-
-func (RationalSet) String() string {
-	return "Q"
-}
-
-func (NaturalSet) String() string {
-	return "N"
-}
-
-func (NaturalSet) Contains(f *fraction) bool {
-	if !f.IsInt() {
-		return false
+func (set *UnionSet) String() string {
+	if set.customName != "" {
+		return set.customName
 	}
-	i, _ := f.Int()
-	return i >= 0
+	s := ""
+	for i, space := range set.sets {
+		if i < len(set.sets)-1 {
+			s += space.String() + " ∪ "
+		} else {
+			s += space.String()
+		}
+	}
+	return s
 }
 
-func (RelativeSet) String() string {
-	return "Z"
+func (set *PeriodicInterval) Contains(f *fraction) bool {
+	if set.interval.Contains(f) {
+		return true
+	}
+
+	if f.GreaterThan(set.interval.upperBound) {
+		for f.GreaterThan(set.interval.upperBound) {
+			f = f.Sub(set.period)
+		}
+		return set.interval.Contains(f)
+	}
+	// here f is necessarily smaller than the lower bound
+	for f.SmallerThan(set.interval.lowerBound) {
+		f = f.Add(set.period)
+	}
+	return set.interval.Contains(f)
 }
 
-func (RelativeSet) Contains(f *fraction) bool {
-	return f.IsInt()
+func (set *PeriodicInterval) String() string {
+	if set.customName != "" {
+		return set.customName
+	}
+	return set.interval.String() + " mod " + set.period.String()
 }

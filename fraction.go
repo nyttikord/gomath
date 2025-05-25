0393d81351b11921f772f184c665d2636981dbf0
Léo-21 <leo.kosman@proton.me>
2025-05-25 19:53:59 +0200

    fix(fraction): actual SmallerThan

--- fraction.go
+++ fraction.go
@@ -117,7 +117,7 @@ func (f fraction) SmallerOrEqualThan(b *fraction) bool {
 }
 
 func (f fraction) SmallerThan(b *fraction) bool {
-	return f.SmallerOrEqualThan(b) && f == *b
+	return f.SmallerOrEqualThan(b) && f != *b
 }
 
 func (f fraction) GreaterOrEqualThan(b *fraction) bool {

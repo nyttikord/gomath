71b72171b1ba70dcd1a8a627124b51905c3cae20
Anhgelus Morhtuuzh <anhgelus@anhgelus.world>
2025-06-04 12:57:00 +0200

    feat(lib): simple result struct

--- main.go
+++ main.go
@@ -1,9 +1,55 @@
 package gomath
 
+import (
+	"errors"
+	"math/big"
+	"strconv"
+	"strings"
+)
+
+var (
+	ErrInvalidResult = errors.New("invalid result")
+)
+
 type Result interface {
 	String() string
-	Approx(uint) string
-	LaTeX() string
+	Approx(int) (string, error)
+	LaTeX() (string, error)
+}
+
+type res struct {
+	*ast
+	result string
+}
+
+func (r *res) String() string {
+	return r.result
+}
+
+func (r *res) Approx(precision int) (string, error) {
+	splits := strings.Split(r.result, "/")
+	if len(splits) == 1 {
+		return r.result, nil
+	} else if len(splits) != 2 {
+		return "", ErrInvalidResult
+	}
+	num, err := strconv.Atoi(splits[0])
+	if err != nil {
+		return "", err
+	}
+	den, err := strconv.Atoi(splits[1])
+	if err != nil {
+		return "", err
+	}
+	return big.NewRat(int64(num), int64(den)).FloatString(precision), nil
+}
+
+func (r *res) LaTeX() (string, error) {
+	err := r.ChangeType(astTypeLatex)
+	if err != nil {
+		return "", err
+	}
+	return r.Body.Eval(&Options{})
 }
 
 // ParseAndCalculate an expression with given Options

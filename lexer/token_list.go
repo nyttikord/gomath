ce132a27714a7ba2eda1198d9bc5db9e842fe6ba
William Hergès <anhgelus@anhgelus.world>
2025-07-26 19:19:19 +0200

    refactor(lexer): introduce token list

---
+++ lexer/token_list.go
@@ -0,0 +1,22 @@
+package lexer
+
+type TokenList struct {
+	index int
+	list  []*Lexer
+}
+
+func (list *TokenList) Current() *Lexer {
+	if list.index < 0 || list.Empty() {
+		return nil
+	}
+	return list.list[list.index]
+}
+
+func (list *TokenList) Next() bool {
+	list.index++
+	return !list.Empty()
+}
+
+func (list *TokenList) Empty() bool {
+	return list.index >= len(list.list)
+}

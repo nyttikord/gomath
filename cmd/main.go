dc2a6b7d1073718dcced43bb745bda6f50167436
William Hergès <anhgelus@anhgelus.world>
2025-07-27 00:01:05 +0200

    feat(cmd): improve readibility of commands in text

--- cmd/main.go
+++ cmd/main.go
@@ -9,14 +9,14 @@ import (
 
 func main() {
 	if len(os.Args) < 2 {
-		fmt.Printf("Usage: %s <subcommand>\nUse %s help for more information.\n", os.Args[0], os.Args[0])
+		fmt.Printf("Usage: %s <subcommand>\nUse '%s help' for more information.\n", os.Args[0], os.Args[0])
 		os.Exit(1)
 	}
 	subcommand := os.Args[1]
 	switch subcommand {
 	case "help":
 		if len(os.Args) != 2 {
-			fmt.Printf("Help does not take any arguments.\n")
+			fmt.Printf("'help' does not take any arguments.\n")
 			os.Exit(1)
 		}
 		fmt.Printf(
@@ -28,7 +28,7 @@ func main() {
 		)
 	case "eval":
 		if len(os.Args) < 3 {
-			fmt.Printf("Usage: %s eval <expression>.\n", os.Args[0])
+			fmt.Printf("Usage: '%s eval <expression>'.\n", os.Args[0])
 			os.Exit(1)
 		}
 		expression := strings.Join(os.Args[2:], " ")
@@ -40,7 +40,7 @@ func main() {
 		fmt.Println(res)
 	case "latex":
 		if len(os.Args) < 3 {
-			fmt.Printf("Usage: %s latex <expression>.\n", os.Args[0])
+			fmt.Printf("Usage: '%s latex <expression>'.\n", os.Args[0])
 			os.Exit(1)
 		}
 		expression := strings.Join(os.Args[2:], " ")
@@ -51,6 +51,6 @@ func main() {
 		}
 		fmt.Println(res)
 	default:
-		fmt.Printf("Unknown subcommand: %s\nUse %s help to more information.\n", subcommand, os.Args[0])
+		fmt.Printf("Unknown subcommand: %s\nUse '%s help' for more information.\n", subcommand, os.Args[0])
 	}
 }

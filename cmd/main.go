5228ea9dd6291e23fcde9da744cbef774b18fce6
William Hergès <william@herges.fr>
2025-08-30 11:47:13 +0200

    feat(cmd): print decimal approximation and flag to set precision

--- cmd/main.go
+++ cmd/main.go
@@ -1,43 +1,63 @@
 package main
 
 import (
+	"flag"
 	"fmt"
 	"github.com/nyttikord/gomath"
 	"os"
 	"strings"
 )
 
+var (
+	precision = uint(6)
+)
+
+func init() {
+	flag.UintVar(&precision, "p", precision, "precision level")
+}
+
 func main() {
-	if len(os.Args) < 2 {
+	flag.Parse()
+	args := flag.Args()
+	if len(args) == 0 {
 		fmt.Printf("Usage: %s <subcommand>\nUse '%s help' for more information.\n", os.Args[0], os.Args[0])
 		os.Exit(1)
 	}
-	subcommand := os.Args[1]
+	subcommand := args[0]
 	switch subcommand {
 	case "help":
-		if len(os.Args) != 2 {
+		if len(args) != 1 {
 			fmt.Printf("'help' does not take any arguments.\n")
 			os.Exit(1)
 		}
 		fmt.Printf(
-			"Usage: %s <subcommand>\nSubcommands\n"+
-				"- help -> print this help text\n"+
-				"- eval <expression> -> evaluate an expression.\n"+
-				"- latex <expression> -> convert an expression to LaTeX code.\n",
+			"Usage: %s [flags] <subcommand>\n\nSubcommands:\n"+
+				"- help               -> print this help text\n"+
+				"- eval <expression>  -> evaluate an expression.\n"+
+				"- latex <expression> -> convert an expression to LaTeX code.\n\n"+
+				"Flags:\n"+
+				"- p uint -> define the precision of the decimal approximation\n",
 			os.Args[0],
 		)
 	case "eval":
-		if len(os.Args) < 3 {
+		if len(args[1:]) == 0 {
 			fmt.Printf("Usage: '%s eval <expression>'.\n", os.Args[0])
 			os.Exit(1)
 		}
-		expression := strings.Join(os.Args[2:], " ")
+		expression := strings.Join(args[1:], " ")
 		res, err := gomath.Parse(expression)
 		if err != nil {
 			fmt.Println(err)
 			os.Exit(2)
 		}
-		fmt.Println(res)
+		fmt.Printf("Exact:   %s\n", res)
+		fmt.Printf("Decimal: %s", res.Approx(int(precision)))
+		if res.IsExact(int(precision)) {
+			fmt.Printf(" (exact)")
+		} else {
+			fmt.Printf(" (not exact)")
+		}
+		fmt.Println()
 	case "latex":
 		if len(os.Args) < 3 {
 			fmt.Printf("Usage: '%s latex <expression>'.\n", os.Args[0])
